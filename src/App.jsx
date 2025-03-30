c6bd92e8022ad53f586f8cef8d15559db3795b80
Hricheak <lincodeintern.12@lincode.ai>
2025-03-31 01:19:03 +0530

    ui updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,22 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import './App.css'
-import Hero from './pages/Hero'
-import Testing from './Testing'
-import FormSection from './pages/FormSection'
-import Footer from './pages/Footer'
+import Hero from "./pages/Hero";
+import Testing from "./Testing";
+import FormSection from "./pages/FormSection";
+import Footer from "./pages/Footer";
 
 function App() {
-
   return (
     <>
-      {/* <Hero /> */}
-<div className='bg-black '>
-
-      <FormSection />
-      <Footer />
-</div>
-
-
-
+      <Hero />
+      <div className="bg-black ">
+        <FormSection />
+        <Footer />
+      </div>
 
       {/* <Testing /> */}
-
-
-      
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
